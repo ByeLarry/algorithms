f4271b0d844d7c8aedbc8a077e3236a90e1b727f
Solovey Roman <120035099+ByeLarry@users.noreply.github.com>
2022-12-10 22:21:47 +0300

    lab 4 complite

--- Solovey/Codes/Алгоритм Куна.py
+++ Solovey/Codes/Алгоритм Куна.py
@@ -1,17 +1,8 @@
-""" class vertex:
-    def __init__(self) -> None:
-        self.color = color
-        self.kolsmej = kolsmej
-        self.smej = smej
-     """
-
-
-
-
 #=====================================================================
 def reading(path): 
     """перевод из текстового файла в двумерный массив типа int"""
     global mat
+    global g 
     data = ''
     f = open(path, 'r')
     trash = f.readline()
@@ -24,78 +15,84 @@ def reading(path):
         if len(trash) == 57:
             break
         mat.append(trash.replace(' \n', '').split())
+        g.append(trash.replace(' \n', '').split())
     i = 0
     while i != len(mat):
         for j in range(len(mat[1])):
             mat[i][j] = int(mat[i][j])
+            g[i][j] = int(mat[i][j])
         i += 1  
 #=====================================================================
 
 
 #=====================================================================
-def dfs2(v, to):
+def dfs2(v, c): #проверка на двудольность
+    global dvydol
     visited[v] = True
-    colors[v] = to
+    colors[v] = c
     for i in range(n):
         if mat[v][i] != 0 or mat[i][v] != 0:
             if not(visited[i]):
-                if to == 1:
+                if c == 1:
                     dfs2(i, 2)
                 else:
                     dfs2(i, 1)
             else:
-                if colors[i] ==to:
+                if colors[i] ==c:
                     dvydol = False
 #=====================================================================
 
 
 #=====================================================================
-def dfs3(v):
+def dfs3(v):# поиск дополняющей цепи
+    global dopsep
     visited[v] = True
     z = 0
     for i in range(n):
         if mat[v][i] != 0:
             if not(visited[i]) and not(dopsep):
                 if par[i] == 1:
-                    if (colors[v] == 2 and t[v] == i) or (colors[v] == 1 and t[i] != v):
+                    if (colors[v] == 2 and t[v] == i+1) or (colors[v] == 1 and t[i] != v+1):
                         dfs3(i)
                         if dopsep:
                             if colors[v] == 2:
-                                if t[v] == i:
+                                if t[v] == i+1:
                                     t[v] = 0
                                     par[v] = 1
                                 else:
                                     if t[v] == 0:
-                                        t[v] = i
+                                        t[v] = i+1
                                         par[v] = 1
                             else:
-                                if t[i] == v:
+                                if t[i] == v+1:
                                     t[i] = 0
                                     par[v] = 1
                                 else:
                                     if t[i] == 0:
-                                        t[i] = v
+                                        t[i] = v+1
                                         par[v] = 1
                 else:
                     dopsep = True
                     par[i] = 1
-                    t[i] = v        
+                    t[i] = v +1       
 #=====================================================================
 
 
 mat = []
+g = []
 path = input('Введите название файла: ') + '.txt'
 reading(path)
 n = len(mat)
 par = [0] * n
-d1 = [0] * n
-d2 = [0] * n
+d1 = [0] * n#вершины 1 доли
+d2 = [0] * n#вершины 2 доли
 t = [0] * n
-colors = [0] * n
+colors = [0] * n #цвета вершин
 a = [0] * n
-dvydol = True
-visited = [False] * len(mat)
-k1 = k2 = 0
+
+visited = [False] * len(mat)#массив посещения вершин
+k1 = 0#кол-во вершин в 1 доле
+k2 = 0#кол-во вершин в 2 доле
 firstv = []
 secondv = []
 for i in range(n):
@@ -108,26 +105,13 @@ for  i in range(n):
         colors[i] = 1
     else:
         colors[i] = 2
-copcolors = colors
-
-#=====================================================================
-g = mat
-for i in range(n):
-    for j in range(n):
-        g[i][j] = 0
-#=====================================================================
-dfs2(0,0)
-
+dvydol = True
+dfs2(0,1)
+if not(dvydol):
+    print('Граф не двудольный')
 if dvydol:
-    #костыль
-    for  i in range(n):
-        if i <= n//2-1:
-            colors[i] = 1
-        else:
-            colors[i] = 2
-    print(colors, 'colors')
     k1, k2 = 0, 0
-    for i in range(n):
+    for i in range(n):#распределение вершин на доли
         d1[i] = 0
         d2[i] = 0
         t[i] = 0
@@ -135,28 +119,22 @@ if dvydol:
         a[i] = 0
         if colors[i] == 1:
             k1 += 1
-            d1[k1-1] = i
-            print(d1, 'd1')
+            d1[k1-1] = i 
             for j in range(n):
                 if mat[i][j] != 0:
                     g[i][j] = 1
         else:
             k2 += 1
-            d2[k2-1] = i
-            print(d2, 'd2')
-    print(d1,'d1')
-    print(d2, 'd2')
-    print(copcolors, 'colors')
-    print(f'Вершины первой доли: ', end='')
+            d2[k2-1] = i 
+    print(f'Вершины первой доли:', end=' ')
     for i in range(k1):
-        print(i, end=' ')
-        print()
-    print(f'Вершины второй доли: ', end='')
+        print(d1[i]+1, end=' ')
+    print()
+    print(f'Вершины второй доли:', end=' ' )
     for i in range(k2):
-        print(i, end=' ')
-        print()
-    #down
-    for i in range(n):
+        print(d2[i]+1, end=' ')
+    print()
+    for i in range(n): #построение паросочетаний
         par[i] = 0
     m = 0
     for i in range(k1):
@@ -166,7 +144,7 @@ if dvydol:
                 par[d1[i]] = 1 
                 par[d2[j]] = 1
                 m += 1
-                t[d2[j]] = d1[i]
+                t[d2[j]] = d1[i] + 1
                 c = 1
         if c == 0:
             dopsep = False
@@ -174,8 +152,8 @@ if dvydol:
                 visited[z] = False
             start = i
             dfs3(d1[i]) 
-    print('Паросочетания: ', end='')
+    print('Паросочетания: ')
     for i in range(n):
         if t[i] != 0:
-            print(f'{t[i]} - {i}')
-    print(t)
+            print(f'{t[i]} - {i+1}')
+    
